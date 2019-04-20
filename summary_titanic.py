98caff62cd61066d9fab12baed20b66b8a7b0ac4
Neel Girish Savla <49500302+nsavla007@users.noreply.github.com>
2019-04-20 15:45:02 -0700

    Update summary_titanic.py

--- summary_titanic.py
+++ summary_titanic.py
@@ -8,5 +8,5 @@ Created on Fri Nov 30 10:41:49 2018
 
 import pandas as pd
 df=pd.read_csv('titanic.csv')
-print(df['fare'].describe())
+print(df['fare'].describe()) #describe is the key word which helps to get summary of the data.
 df['fare'].plot(kind='box')
